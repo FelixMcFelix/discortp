851a0d4a40ae7f6c2c007a6d67ad6fd8941b6aef
Kyle Simpson <kyleandrew.simpson@gmail.com>
2020-05-03 19:24:03 +0100

    Include Discord's IP Discovery

---
+++ src/discord.rs
@@ -0,0 +1,53 @@
+use pnet_macros_support::packet::PrimitiveValues;
+
+include!(concat!(env!("OUT_DIR"), "/discord.rs"));
+
+const IP_DISCOVERY_LEN: usize = IpDiscoveryPacket::minimum_packet_size() + 64;
+
+impl IpDiscoveryPacket<'_> {
+	/// Standard packet length when using Discord-specified lengths.
+	pub const fn const_packet_size() -> usize {
+		IP_DISCOVERY_LEN
+	}
+}
+
+impl MutableIpDiscoveryPacket<'_> {
+	/// Standard packet length when using Discord-specified lengths.
+	pub const fn const_packet_size() -> usize {
+		IP_DISCOVERY_LEN
+	}
+}
+
+/// Packet type for Discord's IP Discovery.
+///
+/// [`Other`] values are illegal.
+///
+/// [`Other`]: #variant.Other
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum IpDiscoveryType {
+	Request,
+	Response,
+	Other(u16),
+}
+
+impl IpDiscoveryType {
+	fn new(val: u16) -> Self {
+		match val {
+			1 => Self::Request,
+			2 => Self::Response,
+			_ => Self::Other(val),
+		}
+	}
+}
+
+impl PrimitiveValues for IpDiscoveryType {
+	type T = (u16,);
+
+	fn to_primitive_values(&self) -> Self::T {
+		match self {
+			Self::Request => (1,),
+			Self::Response => (2,),
+			Self::Other(n) => (*n,),
+		}
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,9 @@
 //!
 //! DiscoRTP was originally developed for use in [Serenity](https://github.com/serenity-rs/serenity).
 
+#[cfg(feature = "discord")]
+#[cfg_attr(docsrs, doc(cfg(feature = "discord")))]
+pub mod discord;
+
 pub mod rtcp;
 pub mod rtp;
