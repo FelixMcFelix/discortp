25e468ee4f8343159d7bab21d6969dfa8e6cbf5b
Kyle Simpson <kyleandrew.simpson@gmail.com>
2020-05-04 11:27:42 +0100

    RTCP/RTP demuxing.
    Should now be able to separate out RTP/RTCP streams (i.e., for Discord).

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-//! DiscoRTP is a lightweight, flexible [Real-time Transport Protocol](https://tools.ietf.org/html/rfc3550)
-//! parsing library designed for use in non-standards compliant environments, such as [Discord](discord.gg).
+//! DiscoRTP is a lightweight, flexible [Real-time Transport Protocol]
+//! parsing library designed for use in non-standards compliant environments, such as [Discord].
 //!
-//! DiscoRTP was originally developed for use in [Serenity](https://github.com/serenity-rs/serenity).
+//! DiscoRTP was originally developed for use in [Serenity].
+//!
+//! [Real-time Transport Protocol]: https://tools.ietf.org/html/rfc3550
+//! [Discord]: https://discord.gg
+//! [Serenity]: https://github.com/serenity-rs/serenity
 
 #[cfg(feature = "discord")]
 #[cfg_attr(docsrs, doc(cfg(feature = "discord")))]
@@ -9,3 +13,73 @@ pub mod discord;
 
 pub mod rtcp;
 pub mod rtp;
+
+use rtcp::{
+	MutableRtcpPacket,
+	RtcpPacket,
+	RtcpType,
+};
+use rtp::{
+	MutableRtpPacket,
+	RtpPacket,
+};
+
+/// RTP/RTCP packets separated from the same stream.
+pub enum Demuxed<'a> {
+	Rtp(RtpPacket<'a>),
+	Rtcp(RtcpPacket<'a>),
+}
+
+/// Mutable RTP/RTCP packets separated from the same stream.
+pub enum DemuxedMut<'a> {
+	Rtp(MutableRtpPacket<'a>),
+	Rtcp(MutableRtcpPacket<'a>),
+}
+
+/// Demultiplexes combined RTP and RTCP streams.
+///
+/// This is subject to the profile restrictions under [RFC 5761],
+/// which restricts the set of allowed payload types. In particular,
+/// this implementation returns an [`RtcpPacket`]
+/// if its packet type matches any known [RTCP packet type].
+///
+/// [RFC 5761]: https://tools.ietf.org/html/rfc5761#section-4
+/// [`RtcpPacket`]: rtcp/struct.RtcpPacket.html
+/// [RTCP packet type]: rtcp/enum.RtcpType.html
+pub fn demux(pkt: &[u8]) -> Option<Demuxed> {
+	if pkt.len() < 2 {
+		None
+	} else {
+		if classify_pt(pkt) {
+			RtpPacket::new(pkt)
+				.map(Demuxed::Rtp)
+		} else {
+			RtcpPacket::new(pkt)
+				.map(Demuxed::Rtcp)
+		}
+	}
+}
+
+/// Demultiplexes combined RTP and RTCP streams, returning mutable packets.
+///
+/// See [`demux`] for more information.
+///
+/// [`demux`]: fn.demux.html
+pub fn demux_mut(pkt: &mut [u8]) -> Option<DemuxedMut> {
+	if pkt.len() < 2 {
+		None
+	} else {
+		if classify_pt(pkt) {
+			MutableRtpPacket::new(pkt)
+				.map(DemuxedMut::Rtp)
+		} else {
+			MutableRtcpPacket::new(pkt)
+				.map(DemuxedMut::Rtcp)
+		}
+	}
+}
+
+// Returns true if RTP.
+fn classify_pt(pkt: &[u8]) -> bool {
+	matches!(RtcpType::new(pkt[1]), RtcpType::Other(_))
+}
--- src/rtcp.rs
+++ src/rtcp.rs
@@ -1,1 +1,110 @@
+use pnet_macros_support::packet::PrimitiveValues;
+
 include!(concat!(env!("OUT_DIR"), "/rtcp.rs"));
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+/// RTCP message types. These define the packet format used for the payload.
+pub enum RtcpType {
+	/// Sender report, containing jitter, reception, timing and volume information.
+	///
+	/// See the relevant [RTP RFC section](https://tools.ietf.org/html/rfc3550#section-6.4.1).
+	/// Unlike the `ReceiverReport`, this includes a `SenderInfo` block.
+	///
+	/// Code 200.
+	SenderReport,
+
+	/// Sender report, containing jitter and reception information.
+	///
+	/// See the relevant [RTP RFC section](https://tools.ietf.org/html/rfc3550#section-6.4.2).
+	///
+	/// Code 201.
+	ReceiverReport,
+
+	/// Source description, mapping SSRC/CCRC values to information about each host.
+	///
+	/// See the relevant [RTP RFC section](https://tools.ietf.org/html/rfc3550#section-6.5).
+	///
+	/// Code 202.
+	SourceDescription,
+
+	/// Source exit message, denoting SSRC/CCRC of exiting hosts and an optional reason string.
+	///
+	/// See the relevant [RTP RFC section](https://tools.ietf.org/html/rfc3550#section-6.6).
+	///
+	/// Code 203.
+	Goodbye,
+
+	/// Application-defined RTCP message, containing a name and arbitrary data.
+	///
+	/// See the relevant [RTP RFC section](https://tools.ietf.org/html/rfc3550#section-6.7).
+	///
+	/// Code 204.
+	ApplicationDefined,
+
+	/// RTPFB, feedback on the transport layer.
+	///
+	/// See [RFC 4845](https://tools.ietf.org/html/rfc4585)
+	///
+	/// Code 205.
+	TransportFeedback,
+
+	/// PSFB, feedback on the payload.
+	///
+	/// See [RFC 4845](https://tools.ietf.org/html/rfc4585)
+	///
+	/// Code 206.
+	PayloadFeedback,
+
+	/// Extended Report message, used for additional/mixed report blocks.
+	///
+	/// See [RTCP XR](https://tools.ietf.org/html/rfc3611).
+	///
+	/// Code 207.
+	ExtendedReport,
+
+	/// Receiver Summary information.
+	///
+	/// See [RFC 5760](https://tools.ietf.org/html/rfc5760).
+	///
+	/// Code 209.
+	ReceiverSummary,
+
+	/// Unknown message type.
+	Other(u8),
+}
+
+impl RtcpType {
+	pub fn new(val: u8) -> Self {
+		match val {
+			200 => Self::SenderReport,
+			201 => Self::ReceiverReport,
+			202 => Self::SourceDescription,
+			203 => Self::Goodbye,
+			204 => Self::ApplicationDefined,
+			205 => Self::TransportFeedback,
+			206 => Self::PayloadFeedback,
+			207 => Self::ExtendedReport,
+			209 => Self::ReceiverSummary,
+			_ => Self::Other(val),
+		}
+	}
+}
+
+impl PrimitiveValues for RtcpType {
+	type T = (u8,);
+
+	fn to_primitive_values(&self) -> Self::T {
+		match self {
+			Self::SenderReport => (200,),
+			Self::ReceiverReport => (201,),
+			Self::SourceDescription => (202,),
+			Self::Goodbye => (203,),
+			Self::ApplicationDefined => (204,),
+			Self::TransportFeedback => (205,),
+			Self::PayloadFeedback => (206,),
+			Self::ExtendedReport => (207,),
+			Self::ReceiverSummary => (209,),
+			Self::Other(val) => (*val,),
+		}
+	}
+}
